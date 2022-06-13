9ee6f038d233a77bfe334dc53520aa5fb6242e1b
Andrey Zhigalov <zigalov.andrey@gmail.com>
2022-06-13 18:32:53 +0400

    Shorted a number of numeric after a dot. Replaced convertation of string numbers from function return to a map filter in a numbers variable

--- js/script.js
+++ js/script.js
@@ -10,19 +10,19 @@ const screen = document.getElementById('screen');
 /* ФУНКЦИЯ КАЛЬКУЛЯТОР И ВЫВОД НА ЭКРАН*/ 
 function inputGetter(i) {
  if (i.target.value == '=') {
-        let numbers = screen.innerHTML.split(/\/|\*|[+-]|\^|√|%/)
-        let operator = screen.innerHTML.split(/[\d\\.]/).join("")
-        return operator == "+"? screen.innerHTML = Number(numbers[0]) + Number(numbers[1]):
-                operator == "-"? screen.innerHTML = Number(numbers[0]) - Number(numbers[1]):
-                operator == "/"? screen.innerHTML = (Number(numbers[0]) / Number(numbers[1])):
-                operator == "*"? screen.innerHTML = Number(numbers[0]) * Number(numbers[1]):
-                operator == "^" && numbers[1] == ""? screen.innerHTML = Math.pow(Number(numbers[0]), 2):
-                operator == "^"? screen.innerHTML = Math.pow(Number(numbers[0]), Number(numbers[1])):
-                operator == "√" && numbers[0] == ""? screen.innerHTML = Math.pow(Number(numbers[1]), (1/2)):
-                operator == "√" && numbers[1] == ""? screen.innerHTML = Math.pow(Number(numbers[0]), (1/2)):
-                operator == "√" ? screen.innerHTML = Number(numbers[0]) * Math.pow(Number(numbers[1]), 1/2):
-                operator == "%" && numbers[1] == "" ? screen.innerHTML = `${(numbers[0] / 100)}%`:
-                operator == "%" && numbers[1] != "" ? screen.innerHTML = `${((Number(numbers[1])/ 100) * Number(numbers[0])).toFixed(2)}`: false;
+        let numbers = screen.innerHTML.split(/\/|\*|[+-]|\^|√|%/).map(i => Number(i));
+        let operator = screen.innerHTML.split(/[\d\\.]/).join("");
+        return operator == "+"? screen.innerHTML = numbers[0] + numbers[1]:
+                operator == "-"? screen.innerHTML = numbers[0] - numbers[1]:
+                operator == "/"? screen.innerHTML = (numbers[0] / numbers[1]).toFixed(2):
+                operator == "*"? screen.innerHTML = (numbers[0] * numbers[1]).toFixed(2):
+                operator == "^" && numbers[1] == ""? screen.innerHTML = Math.pow(numbers[0], 2).toFixed(2):
+                operator == "^"? screen.innerHTML = Math.pow(numbers[0], numbers[1]).toFixed(2):
+                operator == "√" && numbers[0] == ""? screen.innerHTML = Math.pow(numbers[1], (1/2)).toFixed(2):
+                operator == "√" && numbers[1] == ""? screen.innerHTML = Math.pow(numbers[0], (1/2)).toFixed(2):
+                operator == "√" ? screen.innerHTML = (numbers[0] * Math.pow(numbers[1], 1/2)).toFixed(2):
+                operator == "%" && numbers[1] == "" ? screen.innerHTML = (numbers[0] / 100).toFixed(2):
+                operator == "%" && numbers[1] != "" ? screen.innerHTML = ((numbers[1]/ 100) * numbers[0]).toFixed(2): false;
                  
     } else if (i.target.value == 'AC') {
         screen.innerHTML = ""
