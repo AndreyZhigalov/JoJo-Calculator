40bf4140b8d480c08f56aae1310fdfc2b8bcfc40
Andrey Zhigalov <zigalov.andrey@gmail.com>
2022-12-01 09:42:40 +0400

    Рефакторинг функции калькулятора

--- js/jotarolator.js
+++ js/jotarolator.js
@@ -9,71 +9,70 @@ class Calculator {
     constructor(bodyId) {
         this.body = document.querySelector(bodyId);
         this.buttons = this.body.querySelectorAll('.calculator__button');
-        this.screen = this.body.querySelector('#screen');
+        this.screen = this.body.querySelector('#display');
         this.keyboard = this.body.querySelector(".calculator__keyboard");
-        this.animationPoint = this.body.querySelector(".calculator__animation-point")
+        this.animationPoint = this.body.querySelector(".calculator__animation-point");
+        this.allOperants = /[\/\*\-\+\u221a\^\%]$/;
+        this.endsWithOperant = /[\/\*\-\+\u221a\^]+$/;
+        this.multiOperantsRegex = /([\/\*\-\+\u221a\^]{2,})([\/\*\-\+\u221a\^]+)/g;
+        this.negativeIntRegex = /([\/\*\-\+]{1})(\-)(\d[\\.\d]*)/g;
+        this.squereRegex = /(\u221a)(\d[\\.\d]*)/g;
+        this.powerRegex = /(\d[\\.\d]*)(\^)(\d[\\.\d]*)/g;
+        this.percentRegex1 = /(\d*[\\.\d]*||\(\d*[\\.\d]*\*\*\d*[\\.\d]*\)||\(\d*[\\.\d]*\*\*\(1\/2\)\))([\-\+]+)(\d+[\\.\d]*)(%)/g;
+        this.percentRegex2 = /(\d*[\\.\d]*||\(\d*[\\.\d]*\*\*\d*[\\.\d]*\)||\(\d*[\\.\d]*\*\*\(1\/2\)\))([\/\*]+)(\d+[\\.\d]*)(%)/g;
         // ФУНКЦИЯ КАЛЬКУЛЯТОР И ВЫВОД НА ЭКРАН
         this.inputGetter = (i) => {
             if (i.target.value == '=') {
-                let str = this.screen.innerText
-
-                while (str.match(/√\d[\.\d]*/)) { // УЧЁТ ВЗЯТИЯ КОРНЯ ИЗ ЧИСЛА
-                    let matches = str.match(/√\d[\.\d]*/);
-                    let startIndex = matches.index
-                    str = str.slice(0, startIndex) + `(${matches[0].substring(1)}**(1/2))` + str.slice(startIndex + matches[0].length);
-                }
-                while (str.match(/\d[\.\d]*\^\d[\.\d]*/)) { // УЧЁТ ВОЗВЕДЕНИЯ В СТЕПЕНЬ
-                    let matches = str.match(/\d[\.\d]*\^\d[\.\d]*/);
-                    let startIndex = matches.index
-                    str = str.slice(0, startIndex) + `(${matches[0].replace(/\^/gui, "**")})` + str.slice(startIndex + matches[0].length);
-                }
-
-                while (str.match(/\d[\.\d]*[\/\*\-\+]*\d*[\.\d]*%/)) { // УЧЁТ ПРОЦЕНТОВ
-                    let matches = str.match(/\d[\.\d]*[\/\*\-\+]*\d*[\.\d]*%/);
-                    let firstInt = matches[0].match(/\d[\.\d]*/)[0]
-                    let percentInt = matches[0].match(/\d[\.\d]*%/)[0].slice(0, -1)
-                    let operator = matches[0].match(/[\+\-\/\*]/) &&  matches[0].match(/[\+\-\/\*]/)[0]   
-                    let startIndex = matches.index
-
-                    if (!operator && startIndex == 0) {
-                        str = `${percentInt / 100}` + str.slice(matches[0].length);
-                    } else if (operator && operator.match(/[\+\-]+/)) {
-                        str = str.slice(0, startIndex) + firstInt + operator + `${firstInt * (percentInt / 100)}` + str.slice(startIndex + matches[0].length);
-                    } else {
-                        str = str.slice(0, startIndex) + firstInt + operator + `${percentInt / 100}` + str.slice(startIndex + matches[0].length);
-                    }
-                }
-
-                while (str.match(/[\/\*\-\+]-\d+/)) { // УЧЁТ ОТРИЦАТЕЛЬНЫХ ЧИСЕЛ
-                    let matches = str.match(/[\/\*\-\+]-\d[\.\d]*/);
-                    let startIndex = matches.index
-                    str = str.slice(0, startIndex) + matches[0][0] + `(${matches[0].substring(1)})` + str.slice(startIndex + matches[0].length);
-                }
+                this.buttons[18].removeAttribute("disabled")
+                let str = this.screen.innerText.replaceAll(this.squereRegex, "($2**(1/2))")
+                    .replaceAll(this.powerRegex, "($1**$3)")
+                    .replace(/^(\d[\\.\d]*)(%)$/, "$1/100")
+                    .replaceAll(this.percentRegex1, "$1$2($1*($3/100))")
+                    .replaceAll(this.percentRegex2, "($1$2($3/100))")
+                    .replaceAll(this.negativeIntRegex, "$1(0-$3)")
+                    .replaceAll(this.multiOperantsRegex, "$2");
 
                 try { // ФИНАЛЬНЫЙ РАСЧЁТ                    
-                    return this.screen.innerText = (new Function(`return ${str || 0}`))()                        
+                    return this.screen.innerText = 1e10 * (new Function(`return ${str || 0}`))().toFixed(4) / 1e10
                 } catch (error) {
                     return this.screen.innerText = "ゴゴゴゴゴゴゴゴゴゴ"
                 }
 
-            } else if (i.target.value == 'AC') {
-                this.screen.innerText = ""
-            } else if (i.target.value == 'x^') {
-                this.screen.innerText += "^";
-            } else if (i.target.value == '√x') {
-                this.screen.innerText += "√";
-            } else if (i.target.value == 'X') {
-                this.screen.innerText += "*";
-            }
-            else {
-                this.screen.innerText += i.target.value;
+            } else {
+                let value = i.target.value.replace("x^", "^").replace("√x", "√").replace("X", "*")
+                let lastValue = this.screen.innerText.slice(-1)
+
+                if (i.target.value == 'AC') {
+                    this.buttons[18].removeAttribute("disabled")
+                    this.screen.innerText = "0"
+                } else if (lastValue === "0" && value === ".") {
+                    this.screen.innerText += value
+                } else if (lastValue !== value && value === ".") {                    
+                    this.buttons[18].setAttribute("disabled", true)                   
+                    Number.isFinite(+lastValue) ? this.screen.innerText += value : this.screen.innerText += "0.";
+                } else if (lastValue === "." && value === ".") {
+                    this.buttons[18].setAttribute("disabled", true)
+                } else if (this.screen.innerText === "0") {
+                    this.screen.innerText = value
+                } else if (this.allOperants.test(value)) {                   
+                    this.buttons[18].removeAttribute("disabled")
+                    if (value === "-" && this.screen.innerText.match(this.endsWithOperant)?.[0].length < 2) {
+                        this.screen.innerText += value
+                    } else if (value === "+" && lastValue === "+") {
+                        
+                    } else {
+                        this.screen.innerText += value
+                    }
+                } else {
+                    this.screen.innerText += value
+                }
             }
         };
         // ИЗМЕНЕНИЕ РАЗМЕРА ШРИФТА НА ЭКРАНЕ, АНИМАЦИЯ ПРИ ЛОЖНОМ ОТВЕТЕ
         this.resultChecker = () => {
             this.screen.innerText.length > 17 ? this.screen.style.fontSize = "20px" : this.screen.style.fontSize = "25px";
             // ВЫЗОВ СТЭНДА
-            if (this.screen.innerText == "ゴゴゴゴゴゴゴゴゴゴ" ) {
+            if (this.screen.innerText == "ゴゴゴゴゴゴゴゴゴゴ") {
                 this.animationPoint.classList.add("wrongResult");
                 this.keyboard.classList.add("standMessageWrong")
 
@@ -98,7 +97,7 @@ class Calculator {
             let thisFix = this;
             for (let button of this.buttons) {
                 button.addEventListener("click", (value) => { thisFix.inputGetter(value) });
-                button.id == "equal" && button.addEventListener("click", () => { thisFix.resultChecker() });
+                button.id == "equals" && button.addEventListener("click", () => { thisFix.resultChecker() });
             }
         }
     }
